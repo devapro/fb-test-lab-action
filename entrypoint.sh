ab81557cf3dcf7ef5d9ba64f1c3f7fc955e0a2f8
Arsenii Kharlanow <arsenyzp@gmail.com>
2025-03-19 00:12:30 +0100

    test output

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,11 +19,19 @@ project_id=$(cat $service_account_file | jq -r ".project_id")
 gcloud auth activate-service-account --key-file=$service_account_file
 gcloud config set project $project_id
 
-firebase_test_lab_output=$(gcloud beta firebase test android run $arg_spec 2>&1)
+# firebase_test_lab_output=$(gcloud beta firebase test android run $arg_spec 2>&1)
+
+firebase_test_lab_output=$(curl https://example.com 2>&1)
 
 echo "run-log<<EOF" >> $GITHUB_OUTPUT
 echo "$firebase_test_lab_output" >> $GITHUB_OUTPUT
 echo "EOF" >> $GITHUB_OUTPUT
+
+{
+  echo 'FTL_RUN_LOG<<EOF'
+  echo "$firebase_test_lab_output"
+  echo EOF
+} >> "$GITHUB_ENV"
 
 if [ $? -eq 0 ]; then
     echo "Test matrix successfully finished"
