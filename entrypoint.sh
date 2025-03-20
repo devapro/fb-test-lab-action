c78b19aeeef2326b093e4122ebb7eb04bd2dc84b
Arsenii Kharlanow <arsenyzp@gmail.com>
2025-03-20 21:59:11 +0100

    update outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,28 +19,23 @@ project_id=$(cat $service_account_file | jq -r ".project_id")
 gcloud auth activate-service-account --key-file=$service_account_file
 gcloud config set project $project_id
 
-# firebase_test_lab_output=$(gcloud beta firebase test android run $arg_spec 2>&1)
+firebase_test_lab_output=$(gcloud beta firebase test android run $arg_spec 2>&1)
 
-# report_url=$(echo "$firebase_test_lab_output" | awk -F'[][]' '/Test results will be streamed to/ {print $2}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-# # Check if a URL was found
-# if [ -n "$report_url" ]; then
-#   echo "FTL_REPORT_URL=$report_url" >> $GITHUB_OUTPUT
-# else
-#   echo "FTL_ERROR_MESSAGE=\"No test results URL found in the text.\"" >> $GITHUB_OUTPUT
-# fi
+report_url=$(echo "$firebase_test_lab_output" | awk -F'[][]' '/Test results will be streamed to/ {print $2}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+# Check if a URL was found
+if [ -n "$report_url" ]; then
+  echo "FTL_REPORT_URL=$report_url" >> $GITHUB_OUTPUT
+else
+  echo "FTL_ERROR_MESSAGE=\"No test results URL found in the text.\"" >> $GITHUB_OUTPUT
+fi
 
-# gcp_url=$(echo "$firebase_test_lab_output" | awk -F'[][]' '/Raw results will be stored in your GCS bucket at/ {print $2}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-# # Check if a URL was found
-# if [ -n "$gcp_url" ]; then
-#   echo "FTL_GCP_URL=$gcp_url" >> $GITHUB_OUTPUT
-# else
-#   echo "FTL_ERROR_MESSAGE=\"No test results URL found in the text.\"" >> $GITHUB_OUTPUT
-# fi
-
-echo "FTL_REPORT_URL=222" >> $GITHUB_OUTPUT
-echo "FTL_GCP_URL=1111" >> $GITHUB_OUTPUT
-
-echo "FTL_TEST_MESSAGE=\"test\"" >> $GITHUB_OUTPUT
+gcp_url=$(echo "$firebase_test_lab_output" | awk -F'[][]' '/Raw results will be stored in your GCS bucket at/ {print $2}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+# Check if a URL was found
+if [ -n "$gcp_url" ]; then
+  echo "FTL_GCP_URL=$gcp_url" >> $GITHUB_OUTPUT
+else
+  echo "FTL_ERROR_MESSAGE=\"No test results URL found in the text.\"" >> $GITHUB_OUTPUT
+fi
 
 if [ $? -eq 0 ]; then
     echo "Test matrix successfully finished"
