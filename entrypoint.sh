fc951e158accac574468cabfb8aa7130e3eec408
Arsenii Kharlanow <arsenyzp@gmail.com>
2025-03-28 23:33:34 +0100

    final fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,20 +18,13 @@ project_id=$(cat $service_account_file | jq -r ".project_id")
 gcloud auth activate-service-account --key-file=$service_account_file
 gcloud config set project $project_id
 
-#firebase_test_lab_output=$(gcloud firebase test android models list --format=text 2>&1)
 firebase_test_lab_output=$(gcloud beta firebase test android run --format=text $arg_spec 2>&1)
-#gcloud beta firebase test android run --format=text $arg_spec > gcloud_output.log 2>&1
-
-echo "5555"
 
 echo $firebase_test_lab_output
-
-echo "11111"
 
 report_url=$(echo $firebase_test_lab_output | awk -F'[][]' '/Test results will be streamed to/ {print $2}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 # Check if a URL was found
 if [ -n "$report_url" ]; then
-  echo "FTL_REPORT_URL=$report_url"
   echo "FTL_REPORT_URL=$report_url" >> $GITHUB_OUTPUT
 else
   echo "FTL_ERROR_MESSAGE=\"No test results URL found in the text.\"" >> $GITHUB_OUTPUT
@@ -40,7 +33,6 @@ fi
 gcp_url=$(echo $firebase_test_lab_output | awk -F'[][]' '/Raw results will be stored in your GCS bucket at/ {print $2}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 # Check if a URL was found
 if [ -n "$gcp_url" ]; then
-  echo "FTL_GCP_URL=$gcp_url"
   echo "FTL_GCP_URL=$gcp_url" >> $GITHUB_OUTPUT
 else
   echo "FTL_ERROR_MESSAGE=\"No test results URL found in the text.\"" >> $GITHUB_OUTPUT
@@ -57,4 +49,4 @@ rm $service_account_file
 
 echo "FTL_TEST_STATUS=$status" >> $GITHUB_OUTPUT
 
-exit 0
+exit $status
